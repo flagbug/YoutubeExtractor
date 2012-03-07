5f53d943f43021b6f9d0328e96e20c7cc16601fd
Dennis Daume <daume.dennis@gmail.com>
2012-03-07 17:31:56 +0100

    First cleanup

--- YoutubeExtractor/YoutubeExtractor/Mp3AudioExtractor.cs
+++ YoutubeExtractor/YoutubeExtractor/Mp3AudioExtractor.cs
@@ -5,102 +5,100 @@ namespace YoutubeExtractor
 {
     internal class Mp3AudioExtractor : IAudioExtractor
     {
-        string _path;
-        FileStream _fs;
-        List<string> _warnings;
-        List<byte[]> _chunkBuffer;
-        List<uint> _frameOffsets;
-        uint _totalFrameLength;
-        bool _isVBR;
-        bool _delayWrite;
-        bool _hasVBRHeader;
-        bool _writeVBRHeader;
-        int _firstBitRate;
-        int _mpegVersion;
-        int _sampleRate;
-        int _channelMode;
-        uint _firstFrameHeader;
+        readonly FileStream fileStream;
+        readonly List<string> warnings;
+        readonly List<byte[]> chunkBuffer;
+        readonly List<uint> frameOffsets;
+        uint totalFrameLength;
+        bool isVbr;
+        bool delayWrite;
+        bool hasVbrHeader;
+        bool writeVbrHeader;
+        int firstBitRate;
+        int mpegVersion;
+        int sampleRate;
+        int channelMode;
+        uint firstFrameHeader;
+
+        public string Path { get; private set; }
 
         public Mp3AudioExtractor(string path, List<string> warnings)
         {
-            _path = path;
-            _fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read, 65536);
-            _warnings = warnings;
-            _chunkBuffer = new List<byte[]>();
-            _frameOffsets = new List<uint>();
-            _delayWrite = true;
+            this.Path = path;
+            this.fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read, 65536);
+            this.warnings = warnings;
+            this.chunkBuffer = new List<byte[]>();
+            this.frameOffsets = new List<uint>();
+            this.delayWrite = true;
         }
 
         public void WriteChunk(byte[] chunk, uint timeStamp)
         {
-            _chunkBuffer.Add(chunk);
-            ParseMP3Frames(chunk);
-            if (_delayWrite && _totalFrameLength >= 65536)
+            this.chunkBuffer.Add(chunk);
+            this.ParseMp3Frames(chunk);
+
+            if (this.delayWrite && this.totalFrameLength >= 65536)
             {
-                _delayWrite = false;
+                this.delayWrite = false;
             }
-            if (!_delayWrite)
+
+            if (!this.delayWrite)
             {
-                Flush();
+                this.Flush();
             }
         }
 
         public void Finish()
         {
-            Flush();
-            if (_writeVBRHeader)
-            {
-                _fs.Seek(0, SeekOrigin.Begin);
-                WriteVBRHeader(false);
-            }
-            _fs.Close();
-        }
+            this.Flush();
 
-        public string Path
-        {
-            get
+            if (this.writeVbrHeader)
             {
-                return _path;
+                this.fileStream.Seek(0, SeekOrigin.Begin);
+                this.WriteVbrHeader(false);
             }
+
+            this.fileStream.Close();
         }
 
         private void Flush()
         {
-            foreach (byte[] chunk in _chunkBuffer)
+            foreach (byte[] chunk in chunkBuffer)
             {
-                _fs.Write(chunk, 0, chunk.Length);
+                this.fileStream.Write(chunk, 0, chunk.Length);
             }
-            _chunkBuffer.Clear();
+
+            this.chunkBuffer.Clear();
         }
 
-        private void ParseMP3Frames(byte[] buff)
+        private void ParseMp3Frames(byte[] buffer)
         {
-            int[] MPEG1BitRate = new int[] { 0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0 };
-            int[] MPEG2XBitRate = new int[] { 0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0 };
-            int[] MPEG1SampleRate = new int[] { 44100, 48000, 32000, 0 };
-            int[] MPEG20SampleRate = new int[] { 22050, 24000, 16000, 0 };
-            int[] MPEG25SampleRate = new int[] { 11025, 12000, 8000, 0 };
+            var mpeg1BitRate = new[] { 0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0 };
+            var mpeg2XBitRate = new[] { 0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0 };
+            var mpeg1SampleRate = new[] { 44100, 48000, 32000, 0 };
+            var mpeg20SampleRate = new[] { 22050, 24000, 16000, 0 };
+            var mpeg25SampleRate = new[] { 11025, 12000, 8000, 0 };
 
             int offset = 0;
-            int length = buff.Length;
+            int length = buffer.Length;
 
             while (length >= 4)
             {
-                ulong header;
-                int mpegVersion, layer, bitRate, sampleRate, padding, channelMode;
-                int frameLen;
+                int mpegVersion, sampleRate, channelMode;
 
-                header = (ulong)BigEndianBitConverter.ToUInt32(buff, offset) << 32;
+                ulong header = (ulong)BigEndianBitConverter.ToUInt32(buffer, offset) << 32;
+
                 if (BitHelper.Read(ref header, 11) != 0x7FF)
                 {
                     break;
                 }
+
                 mpegVersion = BitHelper.Read(ref header, 2);
-                layer = BitHelper.Read(ref header, 2);
+                int layer = BitHelper.Read(ref header, 2);
                 BitHelper.Read(ref header, 1);
-                bitRate = BitHelper.Read(ref header, 4);
+                int bitRate = BitHelper.Read(ref header, 4);
                 sampleRate = BitHelper.Read(ref header, 2);
-                padding = BitHelper.Read(ref header, 1);
+                int padding = BitHelper.Read(ref header, 1);
                 BitHelper.Read(ref header, 1);
                 channelMode = BitHelper.Read(ref header, 2);
 
@@ -109,97 +107,120 @@ namespace YoutubeExtractor
                     break;
                 }
 
-                bitRate = ((mpegVersion == 3) ? MPEG1BitRate[bitRate] : MPEG2XBitRate[bitRate]) * 1000;
+                bitRate = ((mpegVersion == 3) ? mpeg1BitRate[bitRate] : mpeg2XBitRate[bitRate]) * 1000;
 
-                if (mpegVersion == 3)
-                    sampleRate = MPEG1SampleRate[sampleRate];
-                else if (mpegVersion == 2)
-                    sampleRate = MPEG20SampleRate[sampleRate];
-                else
-                    sampleRate = MPEG25SampleRate[sampleRate];
+                switch (mpegVersion)
+                {
+                    case 2:
+                        sampleRate = mpeg20SampleRate[sampleRate];
+                        break;
 
-                frameLen = GetFrameLength(mpegVersion, bitRate, sampleRate, padding);
-                if (frameLen > length)
+                    case 3:
+                        sampleRate = mpeg1SampleRate[sampleRate];
+                        break;
+
+                    default:
+                        sampleRate = mpeg25SampleRate[sampleRate];
+                        break;
+                }
+
+                int frameLenght = GetFrameLength(mpegVersion, bitRate, sampleRate, padding);
+
+                if (frameLenght > length)
                 {
                     break;
                 }
 
-                bool isVBRHeaderFrame = false;
-                if (_frameOffsets.Count == 0)
+                bool isVbrHeaderFrame = false;
+
+                if (frameOffsets.Count == 0)
                 {
                     // Check for an existing VBR header just to be safe (I haven't seen any in FLVs)
                     int o = offset + GetFrameDataOffset(mpegVersion, channelMode);
-                    if (BigEndianBitConverter.ToUInt32(buff, o) == 0x58696E67)
-                    { // "Xing"
-                        isVBRHeaderFrame = true;
-                        _delayWrite = false;
-                        _hasVBRHeader = true;
-                    }
-                }
 
-                if (isVBRHeaderFrame) { }
-                else if (_firstBitRate == 0)
-                {
-                    _firstBitRate = bitRate;
-                    _mpegVersion = mpegVersion;
-                    _sampleRate = sampleRate;
-                    _channelMode = channelMode;
-                    _firstFrameHeader = BigEndianBitConverter.ToUInt32(buff, offset);
-                }
-                else if (!_isVBR && (bitRate != _firstBitRate))
-                {
-                    _isVBR = true;
-                    if (_hasVBRHeader) { }
-                    else if (_delayWrite)
+                    if (BigEndianBitConverter.ToUInt32(buffer, o) == 0x58696E67)
                     {
-                        WriteVBRHeader(true);
-                        _writeVBRHeader = true;
-                        _delayWrite = false;
-                    }
-                    else
-                    {
-                        _warnings.Add("Detected VBR too late, cannot add VBR header.");
+                        // "Xing"
+                        isVbrHeaderFrame = true;
+                        this.delayWrite = false;
+                        this.hasVbrHeader = true;
                     }
                 }
 
-                _frameOffsets.Add(_totalFrameLength + (uint)offset);
+                if (!isVbrHeaderFrame)
+                {
+                    if (this.firstBitRate == 0)
+                    {
+                        this.firstBitRate = bitRate;
+                        this.mpegVersion = mpegVersion;
+                        this.sampleRate = sampleRate;
+                        this.channelMode = channelMode;
+                        this.firstFrameHeader = BigEndianBitConverter.ToUInt32(buffer, offset);
+                    }
 
-                offset += frameLen;
-                length -= frameLen;
+                    else if (!this.isVbr && (bitRate != this.firstBitRate))
+                    {
+                        this.isVbr = true;
+
+                        if (!this.hasVbrHeader)
+                        {
+                            if (this.delayWrite)
+                            {
+                                this.WriteVbrHeader(true);
+                                this.writeVbrHeader = true;
+                                this.delayWrite = false;
+                            }
+
+                            else
+                            {
+                                this.warnings.Add("Detected VBR too late, cannot add VBR header.");
+                            }
+                        }
+                    }
+                }
+
+                this.frameOffsets.Add(this.totalFrameLength + (uint)offset);
+
+                offset += frameLenght;
+                length -= frameLenght;
             }
 
-            _totalFrameLength += (uint)buff.Length;
+            this.totalFrameLength += (uint)buffer.Length;
         }
 
-        private void WriteVBRHeader(bool isPlaceholder)
+        private void WriteVbrHeader(bool isPlaceholder)
         {
-            byte[] buff = new byte[GetFrameLength(_mpegVersion, 64000, _sampleRate, 0)];
+            var buffer = new byte[GetFrameLength(this.mpegVersion, 64000, this.sampleRate, 0)];
+
             if (!isPlaceholder)
             {
-                uint header = _firstFrameHeader;
-                int dataOffset = GetFrameDataOffset(_mpegVersion, _channelMode);
+                uint header = this.firstFrameHeader;
+                int dataOffset = GetFrameDataOffset(this.mpegVersion, this.channelMode);
                 header &= 0xFFFE0DFF; // Clear CRC, bitrate, and padding fields
-                header |= (uint)((_mpegVersion == 3) ? 5 : 8) << 12; // 64 kbit/sec
-                BitHelper.CopyBytes(buff, 0, BigEndianBitConverter.GetBytes(header));
-                BitHelper.CopyBytes(buff, dataOffset, BigEndianBitConverter.GetBytes((uint)0x58696E67)); // "Xing"
-                BitHelper.CopyBytes(buff, dataOffset + 4, BigEndianBitConverter.GetBytes((uint)0x7)); // Flags
-                BitHelper.CopyBytes(buff, dataOffset + 8, BigEndianBitConverter.GetBytes((uint)_frameOffsets.Count)); // Frame count
-                BitHelper.CopyBytes(buff, dataOffset + 12, BigEndianBitConverter.GetBytes(_totalFrameLength)); // File length
+                header |= (uint)((mpegVersion == 3) ? 5 : 8) << 12; // 64 kbit/sec
+                BitHelper.CopyBytes(buffer, 0, BigEndianBitConverter.GetBytes(header));
+                BitHelper.CopyBytes(buffer, dataOffset, BigEndianBitConverter.GetBytes(0x58696E67)); // "Xing"
+                BitHelper.CopyBytes(buffer, dataOffset + 4, BigEndianBitConverter.GetBytes((uint)0x7)); // Flags
+                BitHelper.CopyBytes(buffer, dataOffset + 8, BigEndianBitConverter.GetBytes((uint)frameOffsets.Count)); // Frame count
+                BitHelper.CopyBytes(buffer, dataOffset + 12, BigEndianBitConverter.GetBytes(totalFrameLength)); // File length
+
                 for (int i = 0; i < 100; i++)
                 {
-                    int frameIndex = (int)((i / 100.0) * _frameOffsets.Count);
-                    buff[dataOffset + 16 + i] = (byte)((_frameOffsets[frameIndex] / (double)_totalFrameLength) * 256.0);
+                    int frameIndex = (int)((i / 100.0) * this.frameOffsets.Count);
+
+                    buffer[dataOffset + 16 + i] = (byte)((this.frameOffsets[frameIndex] / (double)this.totalFrameLength) * 256.0);
                 }
             }
-            _fs.Write(buff, 0, buff.Length);
+
+            this.fileStream.Write(buffer, 0, buffer.Length);
         }
 
-        private int GetFrameLength(int mpegVersion, int bitRate, int sampleRate, int padding)
+        private static int GetFrameLength(int mpegVersion, int bitRate, int sampleRate, int padding)
         {
             return ((mpegVersion == 3) ? 144 : 72) * bitRate / sampleRate + padding;
         }
 
-        private int GetFrameDataOffset(int mpegVersion, int channelMode)
+        private static int GetFrameDataOffset(int mpegVersion, int channelMode)
         {
             return 4 + ((mpegVersion == 3) ?
                 ((channelMode == 3) ? 17 : 32) :
