8196c715309d6e91a2b71b890b4ecb8b9778c0f6
Dennis Daume <daume.dennis@gmail.com>
2012-03-07 13:49:35 +0100

    Added the VideoType property

--- YoutubeExtractor/YoutubeExtractor/DownloadInfo.cs
+++ YoutubeExtractor/YoutubeExtractor/DownloadInfo.cs
@@ -2,9 +2,53 @@
 {
     public class DownloadInfo
     {
+        private int formatCode;
+
         public string DownloadUrl { get; private set; }
 
-        public int FormatCode { get; private set; }
+        public YouTubeVideoType VideoType { get; private set; }
+
+        public int FormatCode
+        {
+            get { return this.formatCode; }
+            set
+            {
+                this.formatCode = value;
+
+                switch (value)
+                {
+                    case 34:
+                    case 35:
+                    case 5:
+                    case 6:
+                        this.VideoType = YouTubeVideoType.Flash;
+                        break;
+
+                    case 18:
+                    case 22:
+                    case 37:
+                    case 38:
+                    case 82:
+                    case 84:
+                        this.VideoType = YouTubeVideoType.Mp4;
+                        break;
+
+                    case 13:
+                    case 17:
+                        this.VideoType = YouTubeVideoType.Mobile;
+                        break;
+
+                    case 43:
+                    case 45:
+                        this.VideoType = YouTubeVideoType.WebM;
+                        break;
+
+                    default:
+                        this.VideoType = YouTubeVideoType.Unknown;
+                        break;
+                }
+            }
+        }
 
         public DownloadInfo(string downloadUrl, int formatCode)
         {
