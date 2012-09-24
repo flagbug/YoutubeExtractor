337adb360bc5eaa5a1cac8c98c3336a75fbda8ed
Dennis Daume <daume.dennis@gmail.com>
2012-09-24 15:14:49 +0200

    Added exception doc

--- YoutubeExtractor/YoutubeExtractor/AudioExtractionException.cs
+++ YoutubeExtractor/YoutubeExtractor/AudioExtractionException.cs
@@ -2,6 +2,9 @@
 
 namespace YoutubeExtractor
 {
+    /// <summary>
+    /// The exception that is thrown when an error occurs durin audio extraction.
+    /// </summary>
     public class AudioExtractionException : Exception
     {
         public AudioExtractionException(string message)
