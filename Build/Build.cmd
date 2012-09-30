2740e333f02a947131d4e97843880fff09f4782e
Dennis Daume <daume.dennis@gmail.com>
2012-09-30 13:49:16 +0200

    Created a simple build script

---
+++ Build/Build.cmd
@@ -0,0 +1,12 @@
+@echo on
+call "%VS110COMNTOOLS%vsvars32.bat"
+
+msbuild.exe /ToolsVersion:4.0 "..\YoutubeExtractor\YoutubeExtractor\YoutubeExtractor.csproj" /p:configuration=Release
+
+mkdir ..\Release
+
+copy ..\YoutubeExtractor\YoutubeExtractor\bin\Release\YoutubeExtractor.dll ..\Release\YoutubeExtractor.dll
+copy ..\YoutubeExtractor\YoutubeExtractor\bin\Release\YoutubeExtractor.xml ..\Release\YoutubeExtractor.xml
+copy ..\Changelog.txt ..\Release\Changelog.txt
+
+pause
