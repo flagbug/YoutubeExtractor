36a4617ae9ca4de807fd138315d4bf70b82f1802
Dennis Daume <daume.dennis@gmail.com>
2013-01-08 03:40:15 +0100

    Updated .nuspec

--- YoutubeExtractor/YoutubeExtractor.WinRT/Properties/AssemblyInfo.cs
+++ YoutubeExtractor/YoutubeExtractor.WinRT/Properties/AssemblyInfo.cs
@@ -1,27 +1,26 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YoutubeExtractor.WinRT")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("YoutubeExtractor is a library for .NET, written in C#, to extract the download link from YouTube videos, download them, and/or extract their audio track.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("YoutubeExtractor.WinRT")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
