fa7082dbc8734b9c7e9736d90f327c20bbf0baaa
Dennis Daume <daume.dennis@gmail.com>
2013-06-26 14:47:26 +0200

    Throw a more specific exception if the uploader didn't made the video available in a specific country

--- YoutubeExtractor/YoutubeExtractor/VideoNotAvailableException.cs
+++ YoutubeExtractor/YoutubeExtractor/VideoNotAvailableException.cs
@@ -7,5 +7,12 @@ namespace YoutubeExtractor
     /// This can happen when the uploader restricts the video to specific countries.
     /// </summary>
     public class VideoNotAvailableException : Exception
-    { }
+    {
+        public VideoNotAvailableException()
+        { }
+
+        public VideoNotAvailableException(string message)
+            : base(message)
+        { }
+    }
 }
