a939e7c0b309575e8fb1eb24556d5f6199c8f8d7
Etienne Charland <mysteryx93@hotmail.com>
2015-06-23 20:40:59 -0500

    Added info about download status for better UI integration

--- YoutubeExtractor/YoutubeExtractor/ProgressEventArgs.cs
+++ YoutubeExtractor/YoutubeExtractor/ProgressEventArgs.cs
@@ -4,8 +4,9 @@ namespace YoutubeExtractor
 {
     public class ProgressEventArgs : EventArgs
     {
-        public ProgressEventArgs(double progressPercentage)
+        public ProgressEventArgs(int progressBytes, double progressPercentage)
         {
+            this.ProgressBytes = progressBytes;
             this.ProgressPercentage = progressPercentage;
         }
 
@@ -13,6 +14,11 @@ namespace YoutubeExtractor
         /// Gets or sets a token whether the operation that reports the progress should be canceled.
         /// </summary>
         public bool Cancel { get; set; }
+
+        /// <summary>
+        /// Gets the progress in bytes downloaded.
+        /// </summary>
+        public int ProgressBytes { get; private set; }
 
         /// <summary>
         /// Gets the progress percentage in a range from 0.0 to 100.0.
--- YoutubeExtractor/YoutubeExtractor/VideoDownloader.cs
+++ YoutubeExtractor/YoutubeExtractor/VideoDownloader.cs
@@ -2,13 +2,11 @@
 using System.IO;
 using System.Net;
 
-namespace YoutubeExtractor
-{
+namespace YoutubeExtractor {
     /// <summary>
     /// Provides a method to download a video from YouTube.
     /// </summary>
-    public class VideoDownloader : Downloader
-    {
+    public class VideoDownloader : Downloader {
         /// <summary>
         /// Initializes a new instance of the <see cref="VideoDownloader"/> class.
         /// </summary>
@@ -17,8 +15,7 @@ namespace YoutubeExtractor
         /// <param name="bytesToDownload">An optional value to limit the number of bytes to download.</param>
         /// <exception cref="ArgumentNullException"><paramref name="video"/> or <paramref name="savePath"/> is <c>null</c>.</exception>
         public VideoDownloader(VideoInfo video, string savePath, int? bytesToDownload = null)
-            : base(video, savePath, bytesToDownload)
-        { }
+            : base(video, savePath, bytesToDownload) { }
 
         /// <summary>
         /// Occurs when the downlaod progress of the video file has changed.
@@ -26,47 +23,45 @@ namespace YoutubeExtractor
         public event EventHandler<ProgressEventArgs> DownloadProgressChanged;
 
         /// <summary>
+        /// Returns the size of the current download in bytes.
+        /// </summary>
+        public int DownloadSize { get; private set; }
+
+        /// <summary>
         /// Starts the video download.
         /// </summary>
         /// <exception cref="IOException">The video file could not be saved.</exception>
         /// <exception cref="WebException">An error occured while downloading the video.</exception>
-        public override void Execute()
-        {
+        public override void Execute() {
             this.OnDownloadStarted(EventArgs.Empty);
 
             var request = (HttpWebRequest)WebRequest.Create(this.Video.DownloadUrl);
 
-            if (this.BytesToDownload.HasValue)
-            {
+            if (this.BytesToDownload.HasValue) {
                 request.AddRange(0, this.BytesToDownload.Value - 1);
             }
 
             // the following code is alternative, you may implement the function after your needs
-            using (WebResponse response = request.GetResponse())
-            {
-                using (Stream source = response.GetResponseStream())
-                {
-                    using (FileStream target = File.Open(this.SavePath, FileMode.Create, FileAccess.Write))
-                    {
+            using (WebResponse response = request.GetResponse()) {
+                using (Stream source = response.GetResponseStream()) {
+                    DownloadSize = (int)response.ContentLength;
+                    using (FileStream target = File.Open(this.SavePath, FileMode.Create, FileAccess.Write)) {
                         var buffer = new byte[1024];
                         bool cancel = false;
                         int bytes;
                         int copiedBytes = 0;
 
-                        while (!cancel && (bytes = source.Read(buffer, 0, buffer.Length)) > 0)
-                        {
+                        while (!cancel && (bytes = source.Read(buffer, 0, buffer.Length)) > 0) {
                             target.Write(buffer, 0, bytes);
 
                             copiedBytes += bytes;
 
-                            var eventArgs = new ProgressEventArgs((copiedBytes * 1.0 / response.ContentLength) * 100);
+                            var eventArgs = new ProgressEventArgs(copiedBytes, (copiedBytes * 1.0 / response.ContentLength) * 100);
 
-                            if (this.DownloadProgressChanged != null)
-                            {
+                            if (this.DownloadProgressChanged != null) {
                                 this.DownloadProgressChanged(this, eventArgs);
 
-                                if (eventArgs.Cancel)
-                                {
+                                if (eventArgs.Cancel) {
                                     cancel = true;
                                 }
                             }
