cec8c1a3ae00e9086ed82e33ec3d3d249842150a
anilgit90 <anilcoll90@gmail.com>
2018-12-17 23:27:07 +0530

    Changes to Project Settings 1. Added Application Icon 2. Exported Project Dependencies to Common Folder 3. Initial Appveyor Script for CI.

--- YoutubeExtractor/YouTubeDownloader/Properties/AssemblyInfo.cs
+++ YoutubeExtractor/YouTubeDownloader/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YouTubeDownloader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Used To Download YouTube Videos")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Home")]
 [assembly: AssemblyProduct("YouTubeDownloader")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
