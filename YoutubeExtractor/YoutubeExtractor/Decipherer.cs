c6a7ce00d7722bb51ccf22b227e0e14c52241cf8
LinPolly <polly19940717@gmail.com>
2020-05-01 13:34:05 +0800

    Fix jsUrl

--- YoutubeExtractor/YoutubeExtractor/Decipherer.cs
+++ YoutubeExtractor/YoutubeExtractor/Decipherer.cs
@@ -9,7 +9,7 @@ namespace YoutubeExtractor
     {
         public static string DecipherWithVersion(string cipher, string cipherVersion)
         {
-            string jsUrl = string.Format("http://s.ytimg.com/yts/jsbin/player{0}.js", cipherVersion);
+            string jsUrl = string.Format("https://www.youtube.com/s/player{0}.js", cipherVersion);
             string js = HttpHelper.DownloadString(jsUrl);
 
             //Find "yv" in this: c&&a.set(b,encodeURIComponent(yv(
